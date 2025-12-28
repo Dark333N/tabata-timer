5aa1d1481603d4c5b01cac8e43ba5ae78677bb7f
Dark333N <75328590+Dark333N@users.noreply.github.com>
2025-12-28 13:17:03 +0100

    trigger pages rebuild

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let rest_time = 20;
 const countdown_audio = new Audio("sound/countdown.mp3");
 const long_beep = new Audio("sound/long_beep.mp3");
 long_beep.volume = 0;
+
 const short_beep1 = new Audio("sound/short_beep.mp3");
 const short_beep2 = new Audio("sound/short_beep.mp3");
 const short_beep3 = new Audio("sound/short_beep.mp3");
@@ -211,3 +212,4 @@ form.addEventListener("submit", (event) => {
     reset();
     render_UI();
 });
+
